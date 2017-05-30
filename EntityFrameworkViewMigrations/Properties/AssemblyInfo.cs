7c55cee305e0d50fe51a7fabb7fee3cef1dcbdcf
Lukáš Kmoch <lukas.kmoch@designeo.cz>
2017-05-30 11:59:32 +0200

    Add solution to repository

---
+++ EntityFrameworkViewMigrations/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EntityFrameworkViewMigrations")]
+[assembly: AssemblyDescription("This nuget package will add support for views migrations to Entity Framework.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Lukáš Kmoc")]
+[assembly: AssemblyProduct("EntityFrameworkViewMigrations")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("374481c9-298e-4d88-bf24-1b26ae67c2ab")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.1.*")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
